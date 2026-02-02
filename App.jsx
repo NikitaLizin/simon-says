e330e726c38d0703e16bd9d79025fb62f6260b95
Nikita <nikita.lizin98@gmail.com>
2026-02-02 21:19:07 +0100

    create a simple simon says game

--- App.jsx
+++ App.jsx
@@ -2,6 +2,9 @@ const app = document.getElementById("root");
 const root = ReactDOM.createRoot(app); 
 
 function Main () {
+
+  
+
  return (
   <div className="main">
     <Game/>
--- components/Game.jsx
+++ components/Game.jsx
@@ -1,10 +1,139 @@
+
+const delay =  ms => (
+  new Promise (resolve => setTimeout(resolve,ms))
+); 
+
+const getStartingSequence =  (arr) => {
+  const sequence = []; 
+
+  while (sequence.length < 3) {
+    const randomNr = Math.floor(Math.random() * arr.length); 
+    sequence.push(arr[randomNr])
+  }
+
+  return sequence; 
+}
+
+async function sequenceAnimation (seq) {
+  for (let i = 0; i <=  seq.length-1; i++){
+    document.body.style.backgroundColor = seq[i]; 
+    await delay(1000);
+
+    document.body.style.backgroundColor = "white";
+    // remove delay for the last sequence value. 
+    if (i !=  seq.length-1) await delay(1000);
+    
+  }   
+}
+
+const incorrect = () => {
+  document.body.style.backgroundColor = "#AD343E"; 
+}
+
+async function correct (color)  { 
+  document.body.style.backgroundColor = `${color}`; 
+  await delay(500); 
+  document.body.style.backgroundColor = "white"; 
+  await delay(500); 
+}
+
+function expandSequence (options) {
+  const length = options.length; 
+  const randomNr = Math.floor(Math.random() * length);
+  return options[randomNr];
+}
+
+
+
 function Game () {
+
+  const [level,setLevel] = React.useState(1); 
+  const [pressed,setPressed] = React.useState([]);
+  const [buttons,setButtons] = React.useState([
+    "#A9E5BB",
+    "#FCF6B1",
+    "#C8ADC0",
+    "#3D5A80",
+  ]);
+  const [sequence, setSequence] = React.useState(getStartingSequence(buttons));
+  const [status,setStatus] = React.useState("countdown"); 
+  const test = React.useRef(null); 
+
+  React.useEffect(() => {
+  if (status !== "sequence") return;
+
+    const showSequence = async () => {
+      await sequenceAnimation(sequence);
+      setStatus("start");
+    };
+
+    showSequence();
+  }, [status]);
+
+   
+
+  if (pressed.length === sequence.length) {
+    const newColor = expandSequence(buttons);
+    setSequence((prev => [...prev,newColor]));
+    setPressed([]); 
+    setStatus("countdown");
+    setLevel((prev => prev+1));
+  }
+
+  /* showSequence(sequence); */ 
+
+
+  
+  const handleClick = async (btnPressed) => {
+
+    if (status != "start") return; 
+    
+    const lastIndex = pressed.length; 
+    const seqCompare = sequence[lastIndex]; 
+
+    if (seqCompare === btnPressed){
+      test.current = true; 
+      await correct(btnPressed); 
+      setPressed((prev => [...prev,btnPressed]));
+      test.current = null; 
+    } else {
+      incorrect(); 
+      setStatus("gameOver"); 
+    }
+
+  }
+
+  const handleCountdown = async() => {
+    await delay(1000); 
+    setStatus("sequence");
+  }
+  
+
   return (
-    <div className="game-container">
-      <button className="game-btn">red</button>
-      <button className="game-btn">blue</button>
-      <button className="game-btn">green</button>
-      <button className="game-btn">yellow</button>
-    </div> 
+    <>
+      <Header content= {`Level ${level}`} /> 
+
+      { status === "countdown" &&
+        <Countdown handleCountDown = {handleCountdown} />
+      }    
+      <div className="game-container" >
+
+       
+
+        { // create buttons for the game. 
+          
+
+          buttons.map((val) => (
+            <div className="game-button-container" key={`game-button-container${val}`} >
+              <GameButton click = {handleClick} type={val} key ={`gameButton${val}`} /> 
+            </div>
+          ))
+
+        }
+
+
+      </div>
+    </> 
   )
+  
 }
